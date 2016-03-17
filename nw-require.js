add6305039477673ae6430017a1c33dec6652b0d
Dida Nurwanda <didanurwanda@gmail.com>
2016-03-18 03:02:53 +0700

    fix

--- nw-require.js
+++ nw-require.js
@@ -1,10 +1,3 @@
-/** `css` is a requirejs plugin
-    that loads a css file and inject it into a page.
-    note that this loader will return immediately,
-    regardless of whether the browser had finished parsing the stylesheet.
-    this css loader is implemented for file optimization and depedency managment
- */
-
 define({
     load: function (name, require, load, config) {
         load(window.require(name));
